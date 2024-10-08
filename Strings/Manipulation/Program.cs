831999c272b13602b1d3717659b888db0f8e3b6f
Winifred Rex <77475621+winifredrex@users.noreply.github.com>
2024-10-08 14:03:31 +0100

    changes

--- Strings/Manipulation/Program.cs
+++ Strings/Manipulation/Program.cs
@@ -7,31 +7,44 @@ string str3 = "THIS is a STRING";
 string[] strs = {"one", "two", "three", "four"};
 
 // TODO: Length of a string 
-
+Console.WriteLine($"{str1.Length}");
 
 // TODO: Access individual characters
-
+Console.WriteLine($"{str1[14]}");
 
 // TODO: Iterate over a string like any other sequence of values
-
+foreach(char ch in str1) {
+    Console.Write(ch);
+    if(ch == 'b') {
+        Console.WriteLine();
+        break;
+    }
+}
 
 // TODO: String Concatenation         
 string outstr;
+outstr = string.Concat(strs);
+Console.WriteLine($"{outstr}");
 
 
 // TODO: Joining strings together with Join
+outstr = String.Join('.', strs);
+Console.WriteLine(outstr);
+outstr = String.Join("--", strs);
+Console.WriteLine(outstr);
 
-
-// TODO: String Comparison
 
 // Equals just returns a regular Boolean
-// bool isEqual = str2.Equals(str3);
-// Console.WriteLine($"{isEqual}");
+bool isEqual = str2.Equals(str3);
+Console.WriteLine($"{isEqual}");
 
 // Compare will perform an ordinal comparison and return:
 // < 0 : first string comes before second in sort order
 // 0 : first and second strings are same position in sort order
 // > 0 : first string comes after the second in sort order
-
+int result = String.Compare(str2, "This is a string");
+Console.WriteLine($"{result}");
 
 // TODO: Replacing content
+outstr = str1.Replace("fox", "cat");
+Console.WriteLine($"{outstr}");
