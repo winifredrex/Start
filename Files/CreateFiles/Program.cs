b3ae98ae202f528140f995cb31397746ea9f8663
Winifred Rex <77475621+winifredrex@users.noreply.github.com>
2024-10-14 08:35:28 +0100

    createfiles

--- Files/CreateFiles/Program.cs
+++ Files/CreateFiles/Program.cs
@@ -5,6 +5,19 @@ const string filename = "TestFile.txt";
 
 // TODO: Create a new file - this will overwrite any existing file
 // Use the "using" construct to automatically close the file stream
-
+using (StreamWriter sw = File.CreateText(filename)) {
+    sw.WriteLine("This is a new file");
+}
 
 // TODO: Determine if a file exists
+Console.WriteLine(File.Exists(filename));
+if (File.Exists(filename)) {
+    // TODO: delete a file
+    File.Delete(filename);
+    
+}else{
+    using (StreamWriter sw = File.CreateText(filename)) {
+        sw.WriteLine("This is a new file");
+    }
+}
+Console.WriteLine(File.Exists(filename));
